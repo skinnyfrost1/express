cd732ce731445ddb7e02397902158ad36c54e1f8
DESKTOP-84SKG63\Frost <yingnan.li@stonybrook.edu>
2019-02-22 12:38:17 -0500

    修改了日期格式

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,28 @@ var express = require('express');
 var router = express.Router();
 var name;
 var today;
-
-// var gameboard= ['X','X','X','O','O','O',' ',' ',' '];
-
-
-
-
-// var grids={'0':'a', '1':'b', '2':'c'};
-// var winer= 'No';
-
-var boardgame={
-  grids:['a','b','c'],
-  winer: 'No'
-}
-
-var jfile=JSON.stringify(boardgame);
-
-
-
-
-
+var winner;
+var grid=[' ',' ',' ',' ',' ',' ',' ',' ',' '];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (name == null)
-    res.render('index', { title: 'Tic Tac Toe' });
+  if (name == null){
+    grid=[' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    winner=' ';
+
+    res.render('index', { title: 'Tic Tac Toe'});
+
+  }
   else
-    res.render('index2', { title: 'Tic Tac Toe', date: today, player: name, gb:jfile});
+    res.render('index2', { title: 'Tic Tac Toe', date: today, player: name});
+  // res.render('index2', { title: 'Tic Tac Toe', jfile:jf,
+    //   a0:grid[0],a1:grid[1],a2:grid[2],
+    //   a3:grid[3],a4:grid[4],a5:grid[5],
+    //   a6:grid[6],a7:grid[7],a8:grid[8]});
 });
 
 router.post('/', function(req, res, next) {
+
   name= req.body.player
   today = new Date();
   var dd = today.getDate();
@@ -43,19 +35,16 @@ router.post('/', function(req, res, next) {
   if (mm < 10) {
     mm = '0' + mm;
   }
-  today = mm + '/' + dd + '/' + yyyy;
+  today = yyyy + '-' + dd + '-' + mm;
+
   res.redirect('/ttt');
 });
 
 
 
 
-
-
-
-
 router.get('/play', function(req, res, next) {
-  res.render('ttt', { title: 'Tic Tac Toe', player: name, iboard: gb});
+  res.render('index2', { title: 'Tic Tac Toe', player: name});
 });
 
 
