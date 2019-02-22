4b01e4298cbdc384a07611662b3499de0a5b2027
DESKTOP-84SKG63\Frost <yingnan.li@stonybrook.edu>
2019-02-21 20:47:55 -0500

    first commit.  if you put it on the server, you have to instll jade and express

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var name;
+var today;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tic Tac Toe' });
+  if (name == null)
+    res.render('index', { title: 'Tic Tac Toe' });
+  else
+    res.render('index2', { title: 'Tic Tac Toe', date: today, player: name});
 });
+
+router.post('/', function(req, res, next) {
+  name= req.body.player
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  today = mm + '/' + dd + '/' + yyyy;
+  res.redirect('/ttt');
+});
+
+
+router.get('/play', function(req, res, next) {
+  res.render('ttt', { title: 'Tic Tac Toe', player: name});
+});
+
+
+
+
+
+// router.post('/play/submit', function(req, res, next){
+//   var player= req.params.player;
+//   res.redirect('/ttt');
+//   }
+
+
 
 module.exports = router;
